0cbe5b962ca70c3c64a7c491b2261a94d102c752
Mayank <mayanksingh6006@gmail.com>
2026-03-29 13:43:18 +0530

    added the http handlers

--- internal/handlers/object.go
+++ internal/handlers/object.go
@@ -2,24 +2,85 @@ package handlers
 
 import (
 	"errors"
-	"fmt"
-	"log"
+	"io"
 	"net/http"
+	"os"
 
-	"github.com/ohmayank/hako/internal/services"
+	"github.com/ohmayank/hako/internal/store"
 )
 
-func main() {
-	mux := http.NewServeMux()
+func PutObject(s store.ObjectStore) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		bucket := r.PathValue("bucket")
+		key := r.PathValue("key")
 
-	srv := &http.Server{
-		Addr: ":6000",
-	}
+		if err := s.Put(r.Context(), bucket, key, r.Body); err != nil {
+			if errors.Is(err, store.ErrEmptyBucket) ||
+				errors.Is(err, store.ErrEmptyKey) ||
+				errors.Is(err, store.ErrWrongPath) {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return
+			}
 
-	go func() {
-		log.Printf("listening on port http://localhost%s", srv.Addr)
-		if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
-			log.Fatalf("server error: %v", err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
 		}
-	}()
+
+		w.WriteHeader(http.StatusCreated)
+
+	}
+}
+
+func GetObject(s store.ObjectStore) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		bucket := r.PathValue("bucket")
+		key := r.PathValue("key")
+
+		rc, err := s.Get(r.Context(), bucket, key)
+		if err != nil {
+			if errors.Is(err, store.ErrEmptyBucket) ||
+				errors.Is(err, store.ErrEmptyKey) ||
+				errors.Is(err, store.ErrWrongPath) {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return
+			}
+
+			if errors.Is(err, os.ErrNotExist) {
+				http.Error(w, err.Error(), http.StatusNotFound)
+				return
+			}
+
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		defer rc.Close()
+
+		if _, err := io.Copy(w, rc); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+	}
+}
+
+func DeleteObject(s store.ObjectStore) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		bucket := r.PathValue("bucket")
+		key := r.PathValue("key")
+
+		if err := s.Delete(r.Context(), bucket, key); err != nil {
+			if errors.Is(err, store.ErrEmptyBucket) ||
+				errors.Is(err, store.ErrEmptyKey) ||
+				errors.Is(err, store.ErrWrongPath) {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return
+			}
+
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.WriteHeader(http.StatusNoContent)
+
+	}
 }
